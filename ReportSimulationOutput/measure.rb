5aef5f80196a07d0d1f210e6e33263c7c9736076
Scott Horowitz <scott.horowitz@nrel.gov>
2026-03-17 15:40:06 -0600

    Consolidate code.

--- ReportSimulationOutput/measure.rb
+++ ReportSimulationOutput/measure.rb
@@ -2185,29 +2185,20 @@ class ReportSimulationOutput < OpenStudio::Measure::ReportingMeasure
       shift_values[idx] = false
       next unless apply_ems_shift(timeseries_frequency)
 
-      if meter_names[idx].include? Constants::ObjectTypeWaterHeaterAdjustment
-        # Shift energy use adjustment to align with hot water energy use
-        shift_values[idx] = true
-      elsif meter_names[idx].include? Constants::ObjectTypePanHeater
-        # Shift energy use adjustment to align with HVAC operation and weather
-        shift_values[idx] = true
-      elsif meter_names[idx].include? Constants::ObjectTypeHPDefrostSupplHeat
-        # Shift energy use adjustment to align with HVAC operation and weather
-        shift_values[idx] = true
-      else
-        [Constants::ObjectTypeDSEHeating,
-         Constants::ObjectTypeDSEHeatingHeatPumpBackup,
-         Constants::ObjectTypeDSEHeatingFanPump,
-         Constants::ObjectTypeDSEHeatingHeatPumpBackupFanPump,
-         Constants::ObjectTypeDSECooling,
-         Constants::ObjectTypeDSECoolingFanPump].each do |dse_obj_type|
-          if meter_names[idx].include? dse_obj_type
-            # Shift energy use adjustment to align with HVAC operation
-            shift_values[idx] = true
-          end
+      # Shift energy use adjustments that lag due to EMS
+      [Constants::ObjectTypeWaterHeaterAdjustment,
+       Constants::ObjectTypePanHeater,
+       Constants::ObjectTypeHPDefrostSupplHeat,
+       Constants::ObjectTypeDSEHeating,
+       Constants::ObjectTypeDSEHeatingHeatPumpBackup,
+       Constants::ObjectTypeDSEHeatingFanPump,
+       Constants::ObjectTypeDSEHeatingHeatPumpBackupFanPump,
+       Constants::ObjectTypeDSECooling,
+       Constants::ObjectTypeDSECoolingFanPump].each do |obj_type|
+        if meter_names[idx].include? obj_type
+          shift_values[idx] = true
         end
       end
-      puts "#{meter_names[idx]}: #{shift_values[idx]}"
     end
 
     rows.each_with_index do |row, row_idx|
