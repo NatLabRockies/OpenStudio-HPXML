ff80104be07588b65762cee03db1369e36bef03f
Scott Horowitz <scott.horowitz@nrel.gov>
2026-04-13 18:24:01 -0400

    Fix timeseries heating/cooling setpoints reported when using on-off thermostat deadband model.

--- ReportSimulationOutput/measure.rb
+++ ReportSimulationOutput/measure.rb
@@ -549,11 +549,17 @@ class ReportSimulationOutput < OpenStudio::Measure::ReportingMeasure
       end
 
       # Also report thermostat setpoints
+      # Note that we use the schedule value, rather than the Zone Thermostat Heating (or Cooling) Setpoint Temperature output variable,
+      # because the latter is adjusted when the on-off thermostat deadband model is used.
       heated_zones.each do |heated_zone|
-        Model.add_output_variable(model, key_value: heated_zone.upcase, variable_name: 'Zone Thermostat Heating Setpoint Temperature', reporting_frequency: args[:timeseries_frequency])
+        thermal_zone = model.getThermalZones.find { |z| z.name.to_s == heated_zone }
+        sched_name = thermal_zone.thermostatSetpointDualSetpoint.get.heatingSetpointTemperatureSchedule.get.name.to_s.upcase
+        Model.add_output_variable(model, key_value: sched_name, variable_name: 'Schedule Value', reporting_frequency: args[:timeseries_frequency])
       end
       cooled_zones.each do |cooled_zone|
-        Model.add_output_variable(model, key_value: cooled_zone.upcase, variable_name: 'Zone Thermostat Cooling Setpoint Temperature', reporting_frequency: args[:timeseries_frequency])
+        thermal_zone = model.getThermalZones.find { |z| z.name.to_s == cooled_zone }
+        sched_name = thermal_zone.thermostatSetpointDualSetpoint.get.coolingSetpointTemperatureSchedule.get.name.to_s.upcase
+        Model.add_output_variable(model, key_value: sched_name, variable_name: 'Schedule Value', reporting_frequency: args[:timeseries_frequency])
       end
     end
 
@@ -1185,28 +1191,32 @@ class ReportSimulationOutput < OpenStudio::Measure::ReportingMeasure
       heated_zones = eval(@model.getBuilding.additionalProperties.getFeatureAsString('heated_zones').get)
       heated_zones.each do |heated_zone|
         var_name = 'Temperature: Heating Setpoint'
+        thermal_zone = @model.getThermalZones.find { |z| z.name.to_s == heated_zone }
         if @hpxml_bldgs.size > 1
-          building_id = @model.getThermalZones.find { |z| z.name.to_s == heated_zone }.additionalProperties.getFeatureAsString('BuildingID').get
+          building_id = thermal_zone.additionalProperties.getFeatureAsString('BuildingID').get
           var_name = "Temperature: #{building_id} Heating Setpoint"
         end
         @zone_temps["#{heated_zone} Heating Setpoint"] = ZoneTemp.new
         @zone_temps["#{heated_zone} Heating Setpoint"].name = var_name
         @zone_temps["#{heated_zone} Heating Setpoint"].timeseries_units = 'F'
-        @zone_temps["#{heated_zone} Heating Setpoint"].timeseries_output = get_report_variable_data_timeseries([heated_zone.upcase], ['Zone Thermostat Heating Setpoint Temperature'], 9.0 / 5.0, 32.0, args[:timeseries_frequency])
+        sched_name = thermal_zone.thermostatSetpointDualSetpoint.get.heatingSetpointTemperatureSchedule.get.name.to_s.upcase
+        @zone_temps["#{heated_zone} Heating Setpoint"].timeseries_output = get_report_variable_data_timeseries([sched_name], ['Schedule Value'], 9.0 / 5.0, 32.0, args[:timeseries_frequency])
       end
 
       # Cooling Setpoints
       cooled_zones = eval(@model.getBuilding.additionalProperties.getFeatureAsString('cooled_zones').get)
       cooled_zones.each do |cooled_zone|
         var_name = 'Temperature: Cooling Setpoint'
+        thermal_zone = @model.getThermalZones.find { |z| z.name.to_s == cooled_zone }
         if @hpxml_bldgs.size > 1
-          building_id = @model.getThermalZones.find { |z| z.name.to_s == cooled_zone }.additionalProperties.getFeatureAsString('BuildingID').get
+          building_id = thermal_zone.additionalProperties.getFeatureAsString('BuildingID').get
           var_name = "Temperature: #{building_id} Cooling Setpoint"
         end
         @zone_temps["#{cooled_zone} Cooling Setpoint"] = ZoneTemp.new
         @zone_temps["#{cooled_zone} Cooling Setpoint"].name = var_name
         @zone_temps["#{cooled_zone} Cooling Setpoint"].timeseries_units = 'F'
-        @zone_temps["#{cooled_zone} Cooling Setpoint"].timeseries_output = get_report_variable_data_timeseries([cooled_zone.upcase], ['Zone Thermostat Cooling Setpoint Temperature'], 9.0 / 5.0, 32.0, args[:timeseries_frequency])
+        sched_name = thermal_zone.thermostatSetpointDualSetpoint.get.coolingSetpointTemperatureSchedule.get.name.to_s.upcase
+        @zone_temps["#{cooled_zone} Cooling Setpoint"].timeseries_output = get_report_variable_data_timeseries([sched_name], ['Schedule Value'], 9.0 / 5.0, 32.0, args[:timeseries_frequency])
       end
     end
 
