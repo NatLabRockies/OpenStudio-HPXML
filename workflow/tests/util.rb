4cc257fc02f5bb908f31f17f4964ed71e139fc1f
Scott Horowitz <scott.horowitz@nrel.gov>
2026-05-06 18:46:09 -0600

    Rename sample file names for consistency.

--- workflow/tests/util.rb
+++ workflow/tests/util.rb
@@ -300,8 +300,8 @@ def _verify_outputs(rundir, hpxml_path, results, hpxml, unit_multiplier)
     if coal_files.any? { |f| hpxml_path.include?(f) }
       next if message.include?('No EIA SEDS rate for coal was found for the state of')
     end
-    # Undersized HVAC or poor HVAC install quality or variable speed system max power ratio schedule or realistic HP backup staging
-    if hpxml_path.include?('base-hvac-undersized.xml') || hpxml_path.include?('install-quality') || hpxml_path.include?('max-power-ratio') || (hpxml.header.heat_pump_backup_heating_capacity_increment.to_f > 0)
+    # HVAC is undersized or poor install quality or advanced research features
+    if hpxml_path.include?('base-hvac-undersized.xml') || hpxml_path.include?('install-quality') || hpxml_path.include?('research-features')
       next if message.include?('There are a large number of unmet hours') && message.include?('for heating; this may indicate the heating system is undersized or the presence of large thermostat setbacks.')
       next if message.include?('There are a large number of unmet hours') && message.include?('for cooling; this may indicate the cooling system is undersized or the presence of large thermostat setbacks.')
     end
