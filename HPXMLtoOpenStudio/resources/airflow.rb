c199a6eb57b4a1ee187ef579ebd5ebbaed5caaa6
Scott Horowitz <scott.horowitz@nrel.gov>
2026-05-21 15:46:39 -0600

    Use consistent equations and a little cleanup to comments.

--- HPXMLtoOpenStudio/resources/airflow.rb
+++ HPXMLtoOpenStudio/resources/airflow.rb
@@ -1827,13 +1827,11 @@ module Airflow
 
           # Calculate the apparent sensible effectiveness
           vent_mech_apparent_sens_eff = (t_sup_out - t_sup_in) / (t_exh_in - t_sup_in)
-
         else
-          # The following is derived from (taken from CSA 439, Clause 9.2.1, Eq. 7):
+          # The following is derived from CSA 439, Clause 9.2.1, Eq. 7:
           t_sup_out = t_sup_in + (vent_mech.sensible_recovery_efficiency_adjusted * (t_exh_in - t_sup_in))
 
           vent_mech_apparent_sens_eff = vent_mech.sensible_recovery_efficiency_adjusted
-
         end
 
         # Calculate the supply temperature before the fan
@@ -1867,8 +1865,8 @@ module Airflow
             #    E_THR = (m_sup,fan * (h_sup,out - h_sup,in) - P_sup,fan) / (m_exh,fan * (h_exh,in - h_sup,in) + P_exh,fan)
             h_sup_out = h_sup_in + (vent_mech.total_recovery_efficiency * (m_fan * (h_exh_in - h_sup_in) + p_fan) + p_fan) / m_fan
           else
-            # The following is derived from (taken from CSA 439, Clause 9.2.1, Eq. 7):
-            h_sup_out = h_sup_in - (vent_mech.total_recovery_efficiency_adjusted * (h_sup_in - h_exh_in))
+            # The following is derived from CSA 439, Clause 9.2.1, Eq. 7:
+            h_sup_out = h_sup_in + (vent_mech.total_recovery_efficiency_adjusted * (h_exh_in - h_sup_in))
           end
 
           w_sup_out = Psychrometrics.w_fT_h_SI(t_sup_out, h_sup_out)
