81fe74feaedf465a59c291f0c3e5754be27bb4ec
Scott Horowitz <scott.horowitz@nrel.gov>
2026-07-10 17:35:22 -0600

    Update warning

--- workflow/tests/util.rb
+++ workflow/tests/util.rb
@@ -366,7 +366,7 @@ def _verify_outputs(rundir, hpxml_path, results, hpxml, unit_multiplier)
     # HPWHs
     if hpxml.buildings.any? { |hpxml_bldg| hpxml_bldg.water_heating_systems.count { |wh| wh.water_heater_type == HPXML::WaterHeaterTypeHeatPump } > 0 }
       next if message.include? 'Recovery Efficiency and Energy Factor could not be calculated during the test for standard ratings'
-      next if message.include? 'SimHVAC: Maximum iterations (20) exceeded for all HVAC loops'
+      next if message.include? 'SimHVAC: Maximum iterations (10) exceeded for all HVAC loops'
       next if message.include? 'For object = Coil:WaterHeating:AirToWaterHeatPump:Wrapped'
       next if message.include? 'Enthalpy out of range (PsyTsatFnHPb)'
       next if message.include?('CheckWarmupConvergence: Loads Initialization') && message.include?('did not converge after 25 warmup days')
@@ -414,7 +414,7 @@ def _verify_outputs(rundir, hpxml_path, results, hpxml, unit_multiplier)
     # GSHPs with hard-sized capacities
     if hpxml_path.include? 'house052.xml'
       next if message.include? 'heating capacity is disproportionate (> 20% different) to total cooling capacity' # safe to ignore
-      next if message.include? 'SimHVAC: Maximum iterations (20) exceeded for all HVAC loops'
+      next if message.include? 'SimHVAC: Maximum iterations (10) exceeded for all HVAC loops'
     end
     # Solar thermal systems
     if hpxml.buildings.any? { |hpxml_bldg| hpxml_bldg.solar_thermal_systems.size > 0 }
